08bc1fd70f677282801ad615532bcdf74cd59b22
Заварыкин Глеб ИП-514 <gleb.denerol@gmail.com>
2025-11-12 18:54:27 +0300

    Добавлен тест cache

---
+++ Test.java
@@ -0,0 +1,34 @@
+public class Test {   
+    public static void main(String[] args) {
+
+        Cache<Integer> cache = new Cache<>(3);
+        
+        System.out.println("Тест cache"); 
+        
+        cache.add(1);
+        cache.add(2);
+        cache.add(3);
+        System.out.println("Добавил 1,2,3");
+        
+        System.out.println("Первый: " + cache.getFirst());
+        System.out.println("Последний: " + cache.getLast());
+        System.out.println("Есть 2: " + cache.exists(2));
+        System.out.println("Элемент 1: " + cache.getItemByIndex(1));
+        
+        cache.add(4);
+        System.out.println("\nДобавил 4");
+        System.out.println("Первый: " + cache.getFirst());
+        System.out.println("Есть 1: " + cache.exists(1));
+        
+        cache.remove(2); 
+        System.out.println("\nудалил 2"); 
+        System.out.println("Есть 2: " + cache.exists(2)); 
+
+        Cache<String> stringCache = new Cache<>(2);
+        stringCache.add("Hello");
+        stringCache.add("World");
+        System.out.println("\nТест со строками");  
+        System.out.println("Первый: " + stringCache.getFirst());
+        System.out.println("Последний: " + stringCache.getLast()); 
+    }
+} 
